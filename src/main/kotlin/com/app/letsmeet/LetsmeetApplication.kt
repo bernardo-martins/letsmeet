a70fca66a1011eab4336d5db3bd978b4531b90ff
bernardo-martins <bme.martins@campus.fct.unl.pt>
2025-03-17 23:10:24 +0100

    commit

---
+++ src/main/kotlin/com/app/letsmeet/LetsmeetApplication.kt
@@ -0,0 +1,11 @@
+package com.app.letsmeet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LetsmeetApplication
+
+fun main(args: Array<String>) {
+	runApplication<LetsmeetApplication>(*args)
+}
---
+++ src/test/kotlin/com/app/letsmeet/LetsmeetApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.app.letsmeet
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LetsmeetApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
